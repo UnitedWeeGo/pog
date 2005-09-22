7310f9005fd68494c14ebe85cab1fd6b443f8541
joel <joel@f5b2f315-eeee-0310-b11b-a38819f40176>
2005-09-22 21:41:47 +0000

    split into 2 pages. fixed caching issue. added clear fields button. overall, a better pog.
    git-svn-id: http://subversion.phpobjectgenerator.com/svn/repository/pog@6 f5b2f315-eeee-0310-b11b-a38819f40176

--- index.php
+++ index.php
@@ -1,143 +1,18 @@
-<?
-include "class.object.php";
-include "class.zipfile.php";
-include "class.misc.php";
-include "pogged/misc.php";
-
-if (IsPostback())
+<?php
+session_start();
+if (isset($_SESSION['objectName']))
 {
-	$objectName = GetVariable('object');
-	$attributeList=Array();
-	$typeList=Array();
-	for ($i=1; $i<50; $i++)
-	{
-		if (GetVariable(('fieldattribute_'.$i)))
-		{
-			$attributeList[] = GetVariable(('fieldattribute_'.$i));
-		}
-		if (GetVariable(('type_'.$i)))
-		{
-			$typeList[] = GetVariable(('type_'.$i));
-		}
-	}
-	
-	$object = new Object($objectName,$attributeList,$typeList);
-	
-	$object->BeginObject();
-	$object->CreateConstructor();
-	$object->CreateGetFunction();
-	$object->CreateGetAllFunction();
-	$object->CreateSaveFunction();
-	$object->CreateSaveNewFunction();
-	$object->CreateDeleteFunction();
-	$object->CreateSQLQuery();
-	$object->EndObject();
-	
-	$objectList[]=$object->objectName;
-	
-	$zipfile = new zipfile();
-	// add the subdirectory ... important! 
-	$zipfile -> add_dir("./pogged/"); 
-	
-	
-	$filename = time().".php";
-	$filedata = fopen("./pogged/$filename","w+");
-	fwrite ($filedata, $object -> string);
-	fclose ($filedata); 
-	
-	//read database file
-	$filedata = fopen("./pogged/class.database.php","r");
-	$data = fread($filedata, filesize("./pogged/class.database.php"));
-	fclose($filedata);
-	$zipfile -> add_file($data, "class.database.php");
-	
-	//read configuration file
-	$filedata = fopen("./pogged/configuration.php","r");
-	$data = fread($filedata, filesize("./pogged/configuration.php"));
-	fclose($filedata);
-	$zipfile -> add_file($data, "configuration.php");
-	
-	//read object file;
-	$filedata = fopen("./pogged/$filename","r");
-	$data = fread($filedata, filesize("./pogged/$filename"));
-	fclose($filedata);
-	
-	$zipfile -> add_file($data, $filename);
-	
-	// OR instead of doing that, you can write out the file to the loca disk like this: 
-	$outputFile = $filename.".zip"; 
-	$fd = fopen ("./pogged/$outputFile", "wb"); 
-	$out = fwrite ($fd, $zipfile -> file()); 
-	fclose ($fd); 
-	
-	mail("joelwan@gmail.com", "POG", $object->string,"From:POG@PHPOBJECTGENERATOR.COM");
-
-	?>
-	
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-	<title>Php Object Generator: A free php object relational database code generator</title>
-	<link rel="stylesheet" href="./phpobjectgenerator.css" type="text/css" />
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	</head>
-	<body>
-	<div class="main">
-		<div class="left2">
-			<img src="./aboutphpobjectgenerator.jpg" alt="About Php Object Generator"/><br/><a href="http://www.phpobjectgenerator.com">Php Object Generator</a>, (<a href="http://www.phpobjectgenerator.com">POG</a>) automatically generates tested Object Oriented code that you can use for your PHP4/PHP5 application. Over the years, we've come to realize that a large portion of a PHP programmer's time is wasted on coding the Database Access Layer of an application simply because every application requires different types of objects. 
-			
-			<br/><br/>By generating the Database Access Layer code for you, POG saves you time; Time you can spend on other areas of your project. The easiest way to understand how Php Object Generator works is to give it a try.
-			
-			<br/><br/>POG was written by <a href="http://www.philosophicallies.com" title="Philosophic Allies">Joel Wan</a> and <a href="http://www.faintlight.com" title="Faint Light">Mark Slemko</a>. Designs by <a href="http://www.designyouwill.com" title="Design You Will">Jonathan Easton</a>. 
-			
-			<br/><br/>Drop us a line @ <a href="mailto:pogguys@phpobjectgenerator.com" title="Drop us a line">pogguys@phpobjectgenerator.com</a>	
-		
-			<br/><br/>Want more? there's <a href="http://www.phpobjectgenerator.com/plog" title="php object generator weblog">the POG Weblog</a>.
-		</div><!-- left -->
-		
-		<div class="middle">
-			<div class="header2">
-				
-			</div><!-- header -->
-			<form method="post" action="index.php">
-			<div class="result">
-				<a href="./pogged/<?=$outputFile?>" title="Download Code"><img src="./download.jpg" border="0"/></a>
-			</div><!-- result -->
-			<div class="greybox2">
-				<textarea cols="200" rows="30"><?= $object->string;?></textarea>
-			</div><!-- greybox -->
-			<div class="generate2">
-			</div><!-- generate -->
-			</form>
-		</div><!-- middle -->
-		<div class="right2">
-		<script type="text/javascript"><!--
-google_ad_client = "pub-7832108692498114";
-google_alternate_color = "FFFFFF";
-google_ad_width = 160;
-google_ad_height = 600;
-google_ad_format = "160x600_as";
-google_ad_type = "text";
-google_ad_channel ="";
-google_color_border = "FFFFFF";
-google_color_bg = "FFFFFF";
-google_color_link = "716500";
-google_color_url = "B8B8B8";
-google_color_text = "CCC078";
-//--></script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-		</div>
-	</div><!-- main -->
-
-	</body>
-	</html>
-<?
+	$objectName = $_SESSION['objectName'];
 }
-else
+if (isset($_SESSION['attributeList']))
 {
+	$attributeList = unserialize($_SESSION['attributeList']);
+}
+if (isset($_SESSION['$typeList']))
+{
+	$typeList = unserialize($_SESSION['$typeList']);
+}
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -167,20 +42,12 @@ function AddField()
 		}
 	}
 }
-function RemoveField(qid)
+function ResetFields()
 {
-	var id=qid;
-	var eid = end;
-	trs=document.getElementsByTagName("tr")
+	trs=document.getElementsByTagName("input")
 	for(var w=0;w<trs.length;w++)
 	{
-		var myid = trs[w].id;
-		if(myid[1] == id)
-		{
-			trs[w].style.display="none";
-			var control = document.getElementById(myid);
-			break;
-		}	
+		trs[w].value= "";
 	}
 }
 //]]>
@@ -189,6 +56,8 @@ function RemoveField(qid)
 <link rel="stylesheet" href="./phpobjectgenerator.css" type="text/css" />
 <meta name="description" content="Php Object Generator, (POG) automatically generates tested Object Oriented code that you can use for your PHP4/PHP5 application.  " />
 <meta name="keywords" content="php, code, generator, classes, object-oriented" />
+<META HTTP-EQUIV="expires" VALUE="Thu, 16 Mar 2000 11:00:00 GMT">
+<META HTTP-EQUIV="pragma" CONTENT="no-cache">
 </head>
 <body>
 <div class="main">
@@ -206,27 +75,51 @@ function RemoveField(qid)
 	<div class="middle">
 		<div class="header">
 		</div><!-- header -->
-		<form method="post" action="index.php">
+		<form method="post" action="index2.php">
 		<div class="objectname">
-			<input type="text" name="object" class="i"/>
+			<input type="text" name="object" class="i" value="<?=(isset($objectName)?$objectName:'')?>"/>
 		</div><!-- objectname -->
 		<div class="greybox">
-			<span class="line"><img src="./object2.jpg" width="33" height="29" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute" width="56" height="18"/> <input type="text" name="fieldattribute_1" class="i"></input>  &nbsp;&nbsp;<img src="./type.jpg" width="36" height="18" alt="object attribute"/> <input type="text" name="type_1" class="i"></input></span><br/><br/>
-			<span class="line"><img src="./object2.jpg" width="33" height="29" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute" width="56" height="18"/>  <input type="text" name="fieldattribute_2" class="i"></input> &nbsp;&nbsp;<img src="./type.jpg" width="36" height="18" alt="object attribute"/> <input type="text" name="type_2" class="i"></input></span><br/><br/>
-			<span class="line"><img src="./object2.jpg" width="33" height="29" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute" width="56" height="18"/>  <input type="text" name="fieldattribute_3" class="i"></input> &nbsp;&nbsp;<img src="./type.jpg" width="36" height="18" alt="object attribute"/> <input type="text" name="type_3" class="i"></input></span><br/>
-		<? 
-		for ($j=4; $j<50; $j++)
+			<span class="line"><img src="./object2.jpg" width="33" height="29" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute" width="56" height="18"/> <input type="text" name="fieldattribute_1" class="i" value="<?=(isset($attributeList)&&isset($attributeList[0])?$attributeList[0]:'')?>"></input>  &nbsp;&nbsp;<img src="./type.jpg" width="36" height="18" alt="object attribute"/> <input type="text" name="type_1" class="i" value="<?=(isset($typeList)&&isset($typeList[0])?$typeList[0]:'')?>"></input></span><br/><br/>
+			<span class="line"><img src="./object2.jpg" width="33" height="29" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute" width="56" height="18"/>  <input type="text" name="fieldattribute_2" class="i" value="<?=(isset($attributeList)&&isset($attributeList[1])?$attributeList[1]:'')?>"></input> &nbsp;&nbsp;<img src="./type.jpg" width="36" height="18" alt="object attribute"/> <input type="text" name="type_2" class="i" value="<?=(isset($typeList)&&isset($typeList[1])?$typeList[1]:'')?>"></input></span><br/><br/>
+			<span class="line"><img src="./object2.jpg" width="33" height="29" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute" width="56" height="18"/>  <input type="text" name="fieldattribute_3" class="i" value="<?=(isset($attributeList)&&isset($attributeList[2])?$attributeList[2]:'')?>"></input> &nbsp;&nbsp;<img src="./type.jpg" width="36" height="18" alt="object attribute"/> <input type="text" name="type_3" class="i"  value="<?=(isset($typeList)&&isset($typeList[2])?$typeList[2]:'')?>"></input></span><br/>
+		<?
+		if (isset($attributeList) || isset($typeList))
 		{
-		?>
-			<div style="display:none" id="attribute_<?php print $j?>">
-				<br/><span class="line"><img src="./object2.jpg" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute"/>  <input type="text" name="fieldattribute_<?php print $j?>" class="i" id="fieldattribute_<?php print $j?>"/> &nbsp;&nbsp;<img src="./type.jpg" alt="object attribute"/> <input type="text" name="type_<?php print $j?>" class="i"></input></span><br/>
-			</div><!-- none -->
-		<?	
+			$max = count($typeList);
+			if (count($attributeList)>count($typeList))
+			{
+				$max = count($attributeList);
+			}
+			for ($j=4; $j<= $max; $j++)
+			{
+				echo '<div style="display:block" id="attribute_'.$j.'">
+					<br/><span class="line"><img src="./object2.jpg" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute"/>  <input type="text" name="fieldattribute_'.$j.'" class="i" id="fieldattribute_'.$j.'" value="'.(isset($attributeList)&&isset($attributeList[$j-1])?$attributeList[$j-1]:'').'"/> &nbsp;&nbsp;<img src="./type.jpg" alt="object attribute"/> <input type="text" name="type_'.$j.'" class="i" value="'.(isset($typeList)&&isset($typeList[$j-1])?$typeList[$j-1]:'').'"></input></span><br/>
+				</div>';
+				
+			}
+			for ($j=$max; $j<50; $j++)
+			{
+				echo '<div style="display:none" id="attribute_'.$j.'">
+					<br/><span class="line"><img src="./object2.jpg" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute"/>  <input type="text" name="fieldattribute_'.$j.'" class="i" id="fieldattribute_'.$j.'" value=""/> &nbsp;&nbsp;<img src="./type.jpg" alt="object attribute"/> <input type="text" name="type_'.$j.'" class="i"></input></span><br/>
+				</div>';
+				
+			}
+		}
+		else 
+		{
+			for ($j=4; $j<50; $j++)
+			{
+			
+				echo '<div style="display:none" id="attribute_'.$j.'">
+					<br/><span class="line"><img src="./object2.jpg" alt="object attribute"/><img src="./attribute.jpg" alt="object attribute"/>  <input type="text" name="fieldattribute_'.$j.'" class="i" id="fieldattribute_'.$j.'"/> &nbsp;&nbsp;<img src="./type.jpg" alt="object attribute"/> <input type="text" name="type_'.$j.'" class="i"></input></span><br/>
+				</div>';	
+			}
 		}
 		?>
 		</div><!-- greybox -->
 		<div class="generate">
-			<a href="#" onclick="AddField()"><img src="./addattribute.jpg" border="0" alt="add attribute"/></a>
+			<a href="#" onclick="AddField()"><img src="./addattribute.jpg" border="0" alt="add attribute"/></a> <a href="#" onclick="ResetFields()"><img src="./resetfields.jpg" border="0" alt="reset fields"/></a>
 		</div><!-- generate -->
 		<div class="submit">
 			<input type="image"  src="./generate.jpg" alt="Generate!"/>
@@ -254,8 +147,5 @@ google_color_text = "CCC078";
 	</div>
 </div><!-- main -->
 
-<?
-}
-?>
 </body>
 </html>
---
+++ index2.php
@@ -0,0 +1,150 @@
+<?
+session_start();
+include "class.object.php";
+include "class.zipfile.php";
+include "class.misc.php";
+include "pogged/misc.php";
+
+if (IsPostback())
+{
+	$objectName = GetVariable('object');
+	$attributeList=Array();
+	$typeList=Array();
+	$z=0;
+	for ($i=1; $i<50; $i++)
+	{
+		if (GetVariable(('fieldattribute_'.$i)))
+		{
+			$attributeList[] = GetVariable(('fieldattribute_'.$i));
+			$z++;
+		}
+		if (GetVariable(('type_'.$i)))
+		{
+			$typeList[] = GetVariable(('type_'.$i));
+		}
+	}
+	$object = new Object($objectName,$attributeList,$typeList);
+	
+	$object->BeginObject();
+	$object->CreateConstructor();
+	$object->CreateGetFunction();
+	$object->CreateGetAllFunction();
+	$object->CreateSaveFunction();
+	$object->CreateSaveNewFunction();
+	$object->CreateDeleteFunction();
+	$object->CreateSQLQuery();
+	$object->EndObject();
+	
+	$_SESSION['objectName'] = $objectName;
+	$_SESSION['attributeList'] = serialize($attributeList);
+	$_SESSION['$typeList'] = serialize($typeList);
+	
+	$objectList[]=$object->objectName;
+	
+	$zipfile = new zipfile();
+	// add the subdirectory ... important! 
+	$zipfile -> add_dir("./pogged/"); 
+	
+	
+	$filename = time().".php";
+	$filedata = fopen("./pogged/$filename","w+");
+	fwrite ($filedata, $object -> string);
+	fclose ($filedata); 
+	
+	//read database file
+	$filedata = fopen("./pogged/class.database.php","r");
+	$data = fread($filedata, filesize("./pogged/class.database.php"));
+	fclose($filedata);
+	$zipfile -> add_file($data, "class.database.php");
+	
+	//read configuration file
+	$filedata = fopen("./pogged/configuration.php","r");
+	$data = fread($filedata, filesize("./pogged/configuration.php"));
+	fclose($filedata);
+	$zipfile -> add_file($data, "configuration.php");
+	
+	//read object file;
+	$filedata = fopen("./pogged/$filename","r");
+	$data = fread($filedata, filesize("./pogged/$filename"));
+	fclose($filedata);
+	
+	$zipfile -> add_file($data, $filename);
+	
+	// OR instead of doing that, you can write out the file to the loca disk like this: 
+	$outputFile = $filename.".zip"; 
+	$fd = fopen ("./pogged/$outputFile", "wb"); 
+	$out = fwrite ($fd, $zipfile -> file()); 
+	fclose ($fd); 
+	
+	mail("joelwan@gmail.com", "POG", $object->string,"From:POG@PHPOBJECTGENERATOR.COM");
+
+	?>
+	
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+	<title>Php Object Generator: A free php object relational database code generator</title>
+	<link rel="stylesheet" href="./phpobjectgenerator.css" type="text/css" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	</head>
+	<body>
+	<div class="main">
+		<div class="left2">
+			<img src="./aboutphpobjectgenerator.jpg" alt="About Php Object Generator"/><br/><a href="http://www.phpobjectgenerator.com">Php Object Generator</a>, (<a href="http://www.phpobjectgenerator.com">POG</a>) automatically generates tested Object Oriented code that you can use for your PHP4/PHP5 application. Over the years, we've come to realize that a large portion of a PHP programmer's time is wasted on coding the Database Access Layer of an application simply because every application requires different types of objects. 
+			
+			<br/><br/>By generating the Database Access Layer code for you, POG saves you time; Time you can spend on other areas of your project. The easiest way to understand how Php Object Generator works is to give it a try.
+			
+			<br/><br/>POG was written by <a href="http://www.philosophicallies.com" title="Philosophic Allies">Joel Wan</a> and <a href="http://www.faintlight.com" title="Faint Light">Mark Slemko</a>. Designs by <a href="http://www.designyouwill.com" title="Design You Will">Jonathan Easton</a>. 
+			
+			<br/><br/>Drop us a line @ <a href="mailto:pogguys@phpobjectgenerator.com" title="Drop us a line">pogguys@phpobjectgenerator.com</a>	
+		
+			<br/><br/>Want more? there's <a href="http://www.phpobjectgenerator.com/plog" title="php object generator weblog">the POG Weblog</a>.
+		</div><!-- left -->
+		
+		<div class="middle">
+			<div class="header2">
+				
+			</div><!-- header -->
+			<form method="post" action="index.php">
+			<div class="result">
+				<a href="./pogged/<?=$outputFile?>" title="Download Code"><img src="./download.jpg" border="0"/></a>
+			</div><!-- result -->
+			<div class="greybox2">
+				<textarea cols="200" rows="30"><?= $object->string;?></textarea>
+			</div><!-- greybox -->
+			<div class="generate2">
+			</div><!-- generate -->
+			</form>
+		</div><!-- middle -->
+		<div class="right2">
+		<script type="text/javascript"><!--
+google_ad_client = "pub-7832108692498114";
+google_alternate_color = "FFFFFF";
+google_ad_width = 160;
+google_ad_height = 600;
+google_ad_format = "160x600_as";
+google_ad_type = "text";
+google_ad_channel ="";
+google_color_border = "FFFFFF";
+google_color_bg = "FFFFFF";
+google_color_link = "716500";
+google_color_url = "B8B8B8";
+google_color_text = "CCC078";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+		</div>
+	</div><!-- main -->
+
+	</body>
+	</html>
+<?
+$_POST = null;
+}
+else
+{
+	header("Location:/");
+}
+?>
