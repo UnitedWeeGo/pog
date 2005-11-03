b5a87dc53b12eec76851dcac7ab4531d1fedc410
joel <joel@f5b2f315-eeee-0310-b11b-a38819f40176>
2005-11-03 04:13:53 +0000

    git-svn-id: http://subversion.phpobjectgenerator.com/svn/repository/pog@44 f5b2f315-eeee-0310-b11b-a38819f40176

--- plog/plog/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$txpcfg['db'] = 'phpob_plog';
-$txpcfg['user'] = 'phpob_joelwan';
-$txpcfg['pass'] = 'leojz037';
-$txpcfg['host'] = 'localhost';
-$txpcfg['table_prefix'] = '';
-$txpcfg['txpath'] = '/home/phpob/public_html/plog/plog';
-$txpcfg['dbcharset'] = 'latin1';
-?>
