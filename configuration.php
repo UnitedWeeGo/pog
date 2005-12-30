97ce7f3a38b6a99ff288d4e3d293e9d0d3a86c9c
joel <joel@f5b2f315-eeee-0310-b11b-a38819f40176>
2005-12-30 07:17:59 +0000

    git-svn-id: http://subversion.phpobjectgenerator.com/svn/repository/pog@76 f5b2f315-eeee-0310-b11b-a38819f40176

--- configuration.php
+++
@@ -1,7 +0,0 @@
-<?
-global $configuration;
-$configuration['revisionNumber']="BETA";
-$configuration['versionNumber'] = "1.6";
-$configuration['author'] = "Php Object Generator";
-$configuration['copyright'] = "Free for personal & commercial use. (Offered under the BSD license)";
-?>
--- datatype.sqlite.inc.php
+++
@@ -1,5 +0,0 @@
-<option value="TEXT" <?=(isset($typeList)&&isset($typeList[$dataTypeIndex])&&$typeList[$dataTypeIndex]=="TEXT"?"selected":'')?>>TEXT</option>
-<option value="NUMERIC" <?=(isset($typeList)&&isset($typeList[$dataTypeIndex])&&$typeList[$dataTypeIndex]=="NUMERIC"?"selected":'')?>>NUMERIC</option>
-<option value="INTEGER" <?=(isset($typeList)&&isset($typeList[$dataTypeIndex])&&$typeList[$dataTypeIndex]=="INTEGER"?"selected":'')?>>INTEGER</option>
-<option value="BLOB" <?=(isset($typeList)&&isset($typeList[$dataTypeIndex])&&$typeList[$dataTypeIndex]=="BLOB"?"selected":'')?>>BLOB</option>
-<option value="OTHER">OTHER...</option>
