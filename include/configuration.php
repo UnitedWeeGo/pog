c2a1a800fc17db3596e842e9cb5928a9f9d69529
joel <joel@f5b2f315-eeee-0310-b11b-a38819f40176>
2006-02-13 20:27:56 +0000

    updated main revision to rc2
    git-svn-id: http://subversion.phpobjectgenerator.com/svn/repository/pog@116 f5b2f315-eeee-0310-b11b-a38819f40176

--- include/configuration.php
+++ include/configuration.php
@@ -1,6 +1,6 @@
 <?
 global $configuration;
-$configuration['revisionNumber']="rc1";
+$configuration['revisionNumber']="rc2";
 $configuration['versionNumber'] = "1.6";
 $configuration['author'] = "Php Object Generator";
 $configuration['copyright'] = "Free for personal & commercial use. (Offered under the BSD license)";
