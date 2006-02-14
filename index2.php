ec31e0c7a6096c56824adc915880555e20ab2034
joel <joel@f5b2f315-eeee-0310-b11b-a38819f40176>
2006-02-14 03:00:12 +0000

    git-svn-id: http://subversion.phpobjectgenerator.com/svn/repository/pog@120 f5b2f315-eeee-0310-b11b-a38819f40176

--- index2.php
+++ index2.php
@@ -73,7 +73,7 @@ if (IsPostback())
 
 	$objectList[]=$object->objectName;
 	$_SESSION['objectString'] = $object->string;
-	mail("joelwan@gmail.com", "POG", $object->string, "FROM:POG <POG@phpobjectgenerator.com>");
+	
 	?>
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
