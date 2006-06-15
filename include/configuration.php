8ebda4b6cc6e9f4d547337197fe6c0de25d63279
joel <joel@f5b2f315-eeee-0310-b11b-a38819f40176>
2006-06-15 07:47:02 +0000

    incremented to 2.0.5
    git-svn-id: http://subversion.phpobjectgenerator.com/svn/repository/pog@321 f5b2f315-eeee-0310-b11b-a38819f40176

--- include/configuration.php
+++ include/configuration.php
@@ -1,7 +1,7 @@
 <?
 global $configuration;
 $configuration['soap'] = "http://www.phpobjectgenerator.com/services/soap.php?wsdl";
-$configuration['revisionNumber']=".4";
+$configuration['revisionNumber']=".5";
 $configuration['versionNumber'] = "2.0";
 $configuration['author'] = "Php Object Generator";
 $configuration['copyright'] = "Free for personal & commercial use. (Offered under the BSD license)";
