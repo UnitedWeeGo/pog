e84539791850d4b7e04b8ee28a50990370e2fd58
joel <joel@f5b2f315-eeee-0310-b11b-a38819f40176>
2006-12-30 09:27:37 +0000

    POG 2.6 Release
    git-svn-id: http://subversion.phpobjectgenerator.com/svn/repository/pog@348 f5b2f315-eeee-0310-b11b-a38819f40176

--- tests/php5.1/app/configuration.php
+++ tests/php5.1/app/configuration.php
@@ -15,9 +15,9 @@ $configuration['db_encoding'] = 1;
 
 // edit the information below to match your database settings
 
-$configuration['db']	= 'evaa';		//	<- database name
+$configuration['db']	= '';		//	<- database name
 $configuration['host'] 	= 'localhost';	//	<- database host
-$configuration['user'] 	= 'root';		//	<- database user
-$configuration['pass']	= 'givvinus';		//	<- database password
+$configuration['user'] 	= '';		//	<- database user
+$configuration['pass']	= '';		//	<- database password
 $configuration['port']	= '3306';		//	<- database port
 ?>
